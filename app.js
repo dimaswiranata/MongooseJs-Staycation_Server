0d70015b3c8c66a41ff0b2ee57afb34c51300177
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-08-23 01:38:12 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 
 // import mongoose
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/db_staycation', 
+mongoose.connect('mongodb+srv://dimaswiranata:staycationserver@cluster0.bzzy3.mongodb.net/db_staycation?retryWrites=true&w=majority', 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
