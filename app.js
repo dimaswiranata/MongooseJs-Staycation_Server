3a869b17e02282b1d7eeb98e74e78784b506a9f0
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-08-27 15:59:03 +0700

    add cors for access API

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const flash = require('connect-flash');
 const session = require('express-session');
 
 // import cors untuk akses API
+const cors = require('cors');
 
 // import mongoose
 const mongoose = require("mongoose");
@@ -72,7 +73,7 @@ app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
 
 // cors
-app.use(cors())
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
